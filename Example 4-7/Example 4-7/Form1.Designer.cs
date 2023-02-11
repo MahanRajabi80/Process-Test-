17d06850e8d4fbf219e140164721ee7404a9b71a
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 17:10:28 +0330

    Add files via upload

---
+++ Example 4-7/Example 4-7/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Example_4_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnPaint = new System.Windows.Forms.Button();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(12, 12);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculator";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(12, 41);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(75, 23);
+            this.btnD.TabIndex = 1;
+            this.btnD.Text = "D:";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // btnPaint
+            // 
+            this.btnPaint.Location = new System.Drawing.Point(12, 70);
+            this.btnPaint.Name = "btnPaint";
+            this.btnPaint.Size = new System.Drawing.Size(75, 23);
+            this.btnPaint.TabIndex = 2;
+            this.btnPaint.Text = "Paint";
+            this.btnPaint.UseVisualStyleBackColor = true;
+            this.btnPaint.Click += new System.EventHandler(this.btnPaint_Click);
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(12, 99);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 3;
+            this.btnFile.Text = "File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(93, 102);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(267, 20);
+            this.txtFile.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(168, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter File Path_ Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 173);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.btnPaint);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnCalc);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Example 4-7 (Process Test)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnPaint;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Example 4-7/Example 4-7/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Example_4_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //--------------------------------------------------------------
+        Process p = new Process();
+        //--------------------------------------------------------------
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            p.Close();
+            p.StartInfo.FileName = "calc.exe";
+            p.Start();
+        }
+        //--------------------------------------------------------------
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            p.Close();
+            p.StartInfo.FileName = "D:";
+            p.Start();
+        }
+        //--------------------------------------------------------------
+        private void btnPaint_Click(object sender, EventArgs e)
+        {
+            p.Close();
+            p.StartInfo.FileName = "mspaint.exe";
+            p.Start();
+        }
+        //--------------------------------------------------------------
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            string s = txtFile.Text;
+            p.Close();
+            p.StartInfo.FileName = s;
+            p.Start();
+        }
+    }
+}
